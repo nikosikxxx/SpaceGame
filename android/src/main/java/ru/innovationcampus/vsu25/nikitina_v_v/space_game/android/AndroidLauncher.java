6b973dbcff16be6230821e09ed0d8ab398e39b63
Space <nikver@gmail.com>
2025-11-19 17:12:30 +0300

    first commit

---
+++ android/src/main/java/ru/innovationcampus/vsu25/nikitina_v_v/space_game/android/AndroidLauncher.java
@@ -0,0 +1,18 @@
+package ru.innovationcampus.vsu25.nikitina_v_v.space_game.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import ru.innovationcampus.vsu25.nikitina_v_v.space_game.Main;
+
+/** Launches the Android application. */
+public class AndroidLauncher extends AndroidApplication {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
+        configuration.useImmersiveMode = true; // Recommended, but not required.
+        initialize(new Main(), configuration);
+    }
+}
